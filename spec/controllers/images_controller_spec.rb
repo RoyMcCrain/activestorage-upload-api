260c6c184baedb2495f1630db4c0d988fdb7c3cc
ofuromochi <hayashi.mr@kanamekey.com>
2020-10-21 17:28:05 +0900

    Fix spec

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe ImagesController, type: :controller do
+RSpec.describe "ImagesController", type: :controller do
 
-  context "uploadをたたいたとき" do
-    it "正しい値が返る" do
-      
-    end
-    end
-  end
 end
