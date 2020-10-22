19062bc302c5de58b8defc919e3f74dc70249cdc
ofuromochi <hayashi.mr@kanamekey.com>
2020-10-22 12:48:18 +0900

    Add ext validation

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -2,6 +2,7 @@ module Api
   class ImagesController < ApplicationController
     def upload
       puts "---------------- Uploading ----------------"
+      ext_validate(params[:images])
       image_log = ImageLog.create!(uploaded_at: Time.zone.now)
       rename_image(params[:images])
       image_log.images.attach(params[:images])
@@ -23,6 +24,17 @@ module Api
 
     def validate_gcs
       # TODO: Create validation for gcs connection
+    end
+
+    def ext_validate(images)
+      file_names = images.map(&:original_filename)
+      ext_allowlist = [".png", ".jpg", ".jpeg", ".gif"]
+
+      file_names.each do |v|
+        if ext_allowlist.exclude?(File.extname(v))
+          raise "Invalid extension."
+        end
+      end
     end
   end
 end
