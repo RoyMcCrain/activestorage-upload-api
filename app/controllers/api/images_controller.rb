2fffb9d1830d11aff2f734278c9acc0d65d787ad
ofuromochi <hayashi.mr@kanamekey.com>
2020-10-26 16:20:03 +0900

    rm binding

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -30,7 +30,6 @@ module Api
 
     # 拡張子よりもRubyのFile.extnameメソッドでチェックする
     def ext_validate
-      binding.irb
       file_names = params[:images].map(&:original_filename)
       ext_allowlist = [".png", ".jpg", ".jpeg", ".gif"]
 
