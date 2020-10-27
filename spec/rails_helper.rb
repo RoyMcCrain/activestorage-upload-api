dfcee01ea906326cb2a06740be125c9e813ff361
ofuromochi <hayashi.mr@kanamekey.com>
2020-10-27 15:03:31 +0900

    Add spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "simplecov"
 require "spec_helper"
+require "request_helper"
+
 ENV["RAILS_ENV"] ||= "test"
 
 require File.expand_path("../config/environment", __dir__)
@@ -36,6 +38,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.include RequestHelpers
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
--- spec/requests/images_controller_spec.rb
+++ spec/requests/images_controller_spec.rb
@@ -1,39 +1,56 @@
 require "rails_helper"
 
 RSpec.describe "ImageUploadAPI", type: :request do
-  let(:images) do
+  let(:files) do
     [
-      ActionDispatch::Http::UploadedFile.new(
-        original_filename:  "test_fire.jpg",
-        type:               "image/jpg",
-        tempfile:           "spec/fixtures/files/test_fire.jpg",
+      Rack::Test::UploadedFile.new(
+        Rails.root.join("spec/fixtures/files/test_fire.jpg"),
+        "image/jpg",
       ),
-      ActionDispatch::Http::UploadedFile.new(
-        original_filename:  "test_mini.jpg",
-        type:               "image/jpg",
-        tempfile:           "spec/fixtures/files/test_mini.jpg",
+      Rack::Test::UploadedFile.new(
+        Rails.root.join("spec/fixtures/files/test_mini.jpg"),
+        "image/jpg",
       ),
     ]
   end
 
-  let(:req) { post "/api/images/upload", params: { images: images }, as: :json }
-  let(:init_count) { ImageLog.count }
+  let(:req) { post "/api/images/upload", params: { images: files } }
+  let(:init_count) { ActiveStorage::Attachment.count }
 
-  context "画像アップロードAPIにリクエストしたとき" do
-    xit "データが作成されている" do
-      is_asserted_by { ImageLog.count == init_count }
+  context "複数の画像ファイルをリクエストしたとき" do
+    it "データが作成されている" do
+      is_asserted_by { ActiveStorage::Attachment.count == init_count }
       req
-      # req.set_form(images, "multipart/form-data")
-      is_asserted_by { ImageLog.count == init_count + 1 }
+      is_asserted_by { ActiveStorage::Attachment.count == init_count + 2 }
     end
 
-    xit "ステータス200が返る" do
+    it "ステータス200が返る" do
+      req
       is_asserted_by { response.status == 200 }
     end
   end
 
   context "不正なファイルをアップロードしたとき" do
+    let(:files) do
+      [
+        Rack::Test::UploadedFile.new(
+          Rails.root.join("spec/fixtures/files/sample.xlsx"),
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        ),
+        Rack::Test::UploadedFile.new(
+          Rails.root.join("spec/fixtures/files/test.txt"),
+          "text/plain",
+        ),
+      ]
+    end
+    it "ステータス200が返る" do
+      req
+      is_asserted_by { response.status == 200 }
+    end
     it "エラーメッセージが返る" do
+      req
+      json = json_parser(response.body)
+      is_asserted_by { json[:error] == "Invalid extension."}
     end
   end
 end
